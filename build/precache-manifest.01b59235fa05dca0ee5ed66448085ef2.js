a1f6a3849cdc784b1c3dfccb4afd6fa945adba4a
ccx1010sky <35686804+ccx1010sky@users.noreply.github.com>
2020-04-02 19:54:51 +0100

    build
    npm run build

---
+++ build/precache-manifest.01b59235fa05dca0ee5ed66448085ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac46e5bce7762eecb3ef1652ba848395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29f9f9b24732332a1cb4",
+    "url": "/static/css/main.abca065c.chunk.css"
+  },
+  {
+    "revision": "906cd5a2eda54ab7e1c8",
+    "url": "/static/js/2.df59205b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df59205b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f9f9b24732332a1cb4",
+    "url": "/static/js/main.c712c625.chunk.js"
+  },
+  {
+    "revision": "80524adf7a680459ccea",
+    "url": "/static/js/runtime-main.f264c7c6.js"
+  }
+]);
